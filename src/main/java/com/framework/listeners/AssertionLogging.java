ff14c13a82df117ef52849f6391efacc7427e2cb
madison-vincent <madison.vincent.work@gmail.com>
2021-11-11 17:00:51 -0700

    logging fixed, change softassert object and fix print out

--- src/main/java/com/framework/listeners/AssertionLogging.java
+++ src/main/java/com/framework/listeners/AssertionLogging.java
@@ -3,7 +3,6 @@ package com.framework.listeners;
 import com.framework.base.Common;
 import org.testng.asserts.IAssert;
 import org.testng.asserts.SoftAssert;
-
 import java.io.IOException;
 
 public class AssertionLogging extends SoftAssert {
@@ -11,7 +10,7 @@ public class AssertionLogging extends SoftAssert {
     @Override
     public void onAssertSuccess(IAssert<?> assertCommand) {
         //Printing the assertion description and letting the user know it passed
-        System.err.println("Assertion: " + assertCommand.getMessage() + " <PASSED> ");
+        System.err.println("*** Assertion: " + assertCommand.getMessage() + " <PASSED> ***");
     }
 
     @Override
@@ -23,14 +22,12 @@ public class AssertionLogging extends SoftAssert {
             e.printStackTrace();
         }
 
-
-
         //Printing the assertion description and letting the user know it failed and why it failed
         String suffix =
                 String.format(
                         "Expected [%s] but found [%s]",
                         assertCommand.getExpected().toString(), assertCommand.getActual().toString());
-        System.err.println("Assertion: " + assertCommand.getMessage() + " <FAILED>. " + suffix);
+        System.err.println("*** Assertion: " + assertCommand.getMessage() + " <FAILED> ***" + suffix);
 
     }
 
